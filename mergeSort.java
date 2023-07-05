9a8143460a8cb53d2e8753022f19dd18830c2e6f
Khushboo Singh <41840494+khushboosinghrecs@users.noreply.github.com>
2023-07-05 22:12:00 +0530

    quick sort

--- mergeSort.java
+++ mergeSort.java
@@ -1,7 +1,95 @@
 public class mergeSort{
     public static void main(String[] args) {
+        int arr[] = {89, 20, 40,2, 3, 1};
+        mergeSort(arr);
+        System.out.println("array after sorting");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
         
     }
+    // merging => combininig  
+    //    arr     [2, 1]
+    //              /  \
+    //left =    [2]     [1]  =  right=
+    //          \      /
+    //          [1,  2] =>RESULT
+
+                        // 8 = 3, 16, => 4 (n*logn)
+                        // / \ 
+                        // 4   4
+                        // /\  /\
+                        // 2 2 2 2
+                        // /\
+                        // 1 1
+    // time complexity=> O(n*(log(n)))
+    public static void mergeSort(int arr[]){
+        if(arr.length<=1 ){
+            return;
+        }
+        int mid = arr.length /2;
+        int n= arr.length;
+        int leftarray[] = new int[mid];
+        int i;
+        int rightarray[] = new int[arr.length -mid];
+        for(i =0; i<mid ;i++){
+            leftarray[i] = arr[i];
+        }
+        for(int j = mid; j<n; j++){
+            rightarray[j-mid] = arr[j];
+        }
+        mergeSort(leftarray);
+        mergeSort(rightarray);
+        merge(arr, leftarray, rightarray);
+    }
+    public static void merge(int arr[],int left[],int right[]){
+        int i =0;
+        int j= 0; 
+        int k= 0; 
+        while(i<left.length &&  j<right.length){ 
+            if(left[i]< right[j]){
+                arr[k]= left[i];   
+                i++;// 3=>4
+            }
+            else{
+                arr[k] = right[j];
+                j++; 
+            }
+              k++;
+        }
+        while(i<left.length){  
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j<right.length){ 
+            arr[k]= right[j];
+            j++;
+            k++;
+        }
+    }
+
+    // arr= [1, 2]
+    // mainarra = [1, ]
+// i leftarry index, j right array index, k = mainarry index  
+// arra =length 4, last index =3, array length 5 last index =4
+// arr[0] = left [0] = -1
+// 0th index arr 1st
+// i=0, k=0 => i=1, j=0, k=1
+// arr[0] = -1
+// arr[1] = 1, j=>1, k=2, i =1, j=1, k=2
+// arr[2] = 6 , i=2, j= 1, k=3
+// arr[3] = 7  i=3, j =1, k=4
+// arr[4] = 8 , i=4, j= 1, k=5
+// arr[5] = 9, j= 2, k=6
+// arr[6] = 17 , j= 3, k=7
+// arr[7] = 100, j=4, k=8
+// arr[8] =1001, j=5, k=9
+// arr[9] = 5000, j=6; k=10;
+// arr[10]= 6000, j= 7, k=11
+// left [5, 6, 7, 8], right [1, 2, 3, 4] ,
+// length =4, lenght =4
 
 
     //                   [8, 9, 6, 4, 5, 2]   // mid = 6/2= 3
@@ -17,8 +105,6 @@ public class mergeSort{
                     
 // mid = 3/2 =1
             // leftar [8]
-
-
 //arr1= [2, 3, 4, 5];
 // /int[]  arr2 == new int(arr.lenght); => [ , , , ]
 // for(int i=0; i<arr.lenght; i++){
@@ -49,110 +135,29 @@ public class mergeSort{
     //      rightarr[2] = arr[5]   5 -3 => 2
       // rightarr[0] = arr[i] =>arr[3] => 4
         //  rightarr [6 -3] = rightarr lentth => 3 => [ ,  , ]
-
-
-    public static void mergeSort(int arr[]){
-        // if
-        int mid = arr.length /2;
-        int n= arr.length;
-        int leftarray[] = new int[mid];
-        int i;
-        int rightarray[] = new int[arr.length -mid];
-        for(i =0; i<mid ;i++){
-            leftarray[i] = arr[i];
-        }
-        for(int j = mid; j<n; j++){
-            rightarray[j-mid] = arr[j];
-        }
-        mergeSort(leftarray);
-        mergeSort(rightarray);
-
-        merge(arr, leftarray, rightarray);
-    
-    }
-
-    // merging => combininig 
-    //    mainarry  [2, 1]
-    //leftarr= [2]   [1]  =  rightarr=
-    //          \      /
-    //          [1,  2]
-
-
-      // merging => combininig 
-    //    arr     [2, 1]
-    //              /  \
-    //left =    [2]     [1]  =  right=
-    //          \      /
-    //          [1,  2] =>RESUL
-
     // LEFT.LENTH =1
     // RTGHT.lenth =1
     // left[0] =2
     // right[0]=1
     // arr = [2, 1];
     // arr[0] = [1 ,1]
-
     // arr=[4, 7, 3]
     // left [4] , right [7, 3] => [3, 7] 
     //              left [7] right [3]
-
-
     // arr [ 8, 5, 4, 7, 3]    left [4 , 5]     right [3, 7, 8]
-
     //  left [4, 7, 6, 9]  right [1, 8, 17, 100, 1001]
     // [3, 4, 5, 7, 8]
     //  [] 
-
-
-    
     // mainarr[6, 4, 9, 7, 10001, 8, 17, 100, 1]
-    //  left [4, 7, 6, 9]  right [1, 8, 17, 100, 1001]
-    public static void merge(int arr[],int left[],int right[]){
+    //  left [4, 6, 7, 9] , right [1, 8, 17, 100, 1001]
+    // two array arr1=[1, 2,3] arr2= [3, 4, 5] , arr =[1, ,2 ,3, 3, 4, 5]
+    //  mainarr[6, 4, 9, 7, 10001, 8, 17, 100, 1]
+    //  left [4, 6, 7, 9] , right [1, 8, 17, 100, 1001, 5000, 6000]
+    // merging => combininig 
+    //    mainarry  [2, 1]
+    //leftarr= [2]   [1]  =  rightarr=
+    //          \      /
+    //          [1,  2]
 
-        int i =0;
-        int j= 0; 
-        int k= 0;
-        
-
-        // i leftarry index, j right array index, k = mainarry index
-        // left [4 , 5]     right [3, 7, 8]
-        // 0< 2   && 0 <3
-        // arr[0] - minval
-        // arr[1]  
-
-        while(i<left.length &&  j<right.length){  // i=0 0<1 && 0<1
-            if(left[i]< right[j]){   // left[0] <right[0] => 2 < 1
-                arr[k]= left[i];     // arr[1] = left[0] =>  4 =  arr[2] => 5
-                i++;   // 0=>1 => 2
-            }
-            else{
-                arr[k] = right[j];     // arr[0] =right[0] arr[0] 3
-                j++; // 0=>1
-            }
-            k++; // 0=>1  => 2  => 3
-        }
-// i 2<2
-        while(i<left.length){  // i =0 arr[1, 1]
-            arr[k] = left[i]; // k =1 arr[1] = left[0] => 2
-            i++;        // 0 => 1
-            k++;         //1 => 2
-        }
-
-        // arr[0] = 3  j=0  =>1
-        // arr[1] = 4
-        // arr[2] = 5
-        //  j =1
-        // j=2
-        // arr[3]= 7
-        // arr[4] =8
-
-        while(j<right.length){ // j =1 1<1
-            arr[k]= right[j];
-            j++;
-            k++;
-        }
-    }
-
-    // arr= [1, 2]
 
 }
---
+++ quicksort.java
@@ -0,0 +1,102 @@
+
+// Quick sort => sorting algorithm, its also a divide and conquer algorithm . it takes the array arr, lowest index as low and higher index as high, to define the range of elements to sort, the partition method is used to find the pivot element and place it at the correct position , so that all elements before pivot element is smaller and after it  is larger 
+
+// pivot = 5;
+
+// [4, 1, 3, 5, 8, 9]
+// 3
+
+// tiem complexity of (quicksort )=> O(n*log(n));
+
+// 
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class quicksort {
+    public static void main(String[] args) {
+
+        int arr[] = {4, 8, 9, 1, 3, 5};
+         int arr1[] = {4, 8, 9, 1, 3, 5};
+         Arrays.sort(arr1);
+        int low =0;
+        int high = arr.length -1;
+        quickSort(arr, low, high);
+    }
+
+
+    //  //   int arr[] = {4, 8, 9, 1, 3, 5}; => pivotindex = 3 => array change =    int arr[] = [4, 1, 3, 5,  8, 9]
+
+
+    // int arr[] =          [4, 1, 3, 5,  8, 9];   =>log(6) =>2
+    //                            /       \
+    //                           /         \
+//  (0, 2)                [4, 1, 3]       [8, 9]  (4, 5)
+//                        [1, 3, 4]              /\
+//  pivotindex=1                  pivotindex= 5
+//                           / \                     /\
+//                          /   \                    /  \
+//               (0, 0)  [1]       [4](2, 2) ,(4, 4) [8] , (6, 5)
+//   
+//              [1, 3, 4, 8, 9]
+    public static void quickSort( int arr[], int low, int high){
+        if(low < high){
+            int pivotindex = partition(arr, low, high);
+            // print =1
+            quickSort(arr, low, pivotindex -1);
+            quickSort(arr, pivotindex+1, high);
+            //  [4, 1, 3, 5,  8, 9]
+            //  quickSort(arr, low, 2);
+            // quickSort(arr, 4, 5)
+        }
+    } 
+    //   int arr[] = {4, 8, 9, 1, 3, 5};
+    // low  =0,  high =5
+    // pivot = arr[5] = 5;
+    // i = low-1 => -1
+    // j=low;  j=0; if(arr[0] <pivot){i++=> 0, temp = arr[0] =>4, arr[0] = 4, arr[0] = arr[0] }
+    //  j=1, if(arr[1]< pivot){}
+    // j=2 if(arr[2]< pivot){}{}
+    // j=3 if(arr[3]<pivot){ i=> 1, temp = arr[1] =>8, arr[1] =1, arr[3] =8 }  = {4, 1, 9, 8, 3, 5};
+
+    // j=4, if(arr[4]<pivot) {i =>2, temp = arr[2] =>9, arr[2]= arr[4] => 3,, arr[4] = 9} =>>[4, 1, 3, 9 ,8, 5]
+    // j=5 , if(arr[5]<pivot) {}
+    // i=>2
+
+    // temp = arr[3] =>9
+    //arr[3]= arr[high] => 5;
+    // arr[high] = temp => 9 => [4, 1, 3, 5,  8, 9]
+
+
+
+
+    //  [4, 1, 3]  
+    // pivot = 3, i =-1
+    // j=0{}
+    // j=1{ i=>0, temp = 4, arr[0] =arr[1] =>1, arr[1]= 4} => [1,4, 3]
+    //  temp =4, arr[1] = 3, arr[2]= 4 => [1, 3, 4]
+    // 1
+
+    //         [4, 1, 3, 5,  8, 9];
+                 //                [4, 1, 3],   0,        2
+                //                   [8, 9]  (4, 5)
+    public static int partition(int[] arr, int low, int high ){
+         int pivot = arr[high];
+         int i= low-1; //3
+
+         for(int j = low; j<high; j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+         }
+
+         int temp = arr[i+1];
+         arr[i+1] = arr[high];
+         arr[high] = temp;
+        return i+1;
+
+    }
+}
