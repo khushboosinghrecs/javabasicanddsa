ac5d850fd77335b615c70db23d04ddff3041d29d
Khushboo Singh <41840494+khushboosinghrecs@users.noreply.github.com>
2023-07-20 22:01:32 +0530

    singly linkedlist

--- BinarySearch.java
+++ BinarySearch.java
@@ -17,93 +17,93 @@ public class BinarySearch {
      else{
           System.out.println("Element is there");
      }
+    }
 
-    //  char arrr[] = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
-            //          0    1    2    3    4    5    6    7    8
-        // target element = 'c';
-        //  int left =0;
-        //  int right = 8;
-        //  mid = 0 + (8-0)/2 =>4
-        // if(e==c)
-        // if(e> c){}
-        // else{ right mid -1}; = mid4 =>3
+    // char arrr[] = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
+    // 0 1 2 3 4 5 6 7 8
+    // target element = 'c';
+    // int left =0;
+    // int right = 8;
+    // mid = 0 + (8-0)/2 =>4
+    // if(e==c)
+    // if(e> c){}
+    // else{ right mid -1}; = mid4 =>3
 
-        // right = 3
+    // right = 3
 
-        // left =0 < 3
-        // mid = 0 + (3-0)/2 =1 
-        // if(b =='c')
-        // (c> b){ left =1+1 =>} left = 2
+    // left =0 < 3
+    // mid = 0 + (3-0)/2 =1
+    // if(b =='c')
+    // (c> b){ left =1+1 =>} left = 2
 
-        // 2<=3{
-            //  5/4 (left +rigt)/ 2
-        //     mid = 2 +(right -left)/2= 2+ (3-2)/2 = 2+ 0;
+    // 2<=3{
+    // 5/4 (left +rigt)/ 2
+    // mid = 2 +(right -left)/2= 2+ (3-2)/2 = 2+ 0;
 
-        //     if(c==c){
-        //         return mid;
-        //     }
-        // }
-//    //  char arrr[] = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
-         //            0    1    2    3    4    5    6    7    8
-        // target element = 'h';
-        // left =0; right = 8;
+    // if(c==c){
+    // return mid;
+    // }
+    // }
+    // // char arrr[] = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
+    // 0 1 2 3 4 5 6 7 8
+    // target element = 'h';
+    // left =0; right = 8;
     // 0<8 mid= 0+ 8-0/2 =4;
-    //  left = 5, right =8
+    // left = 5, right =8
     // 5<=8
-    // mid =5+ 8-5 => 5+1 =6 
+    // mid =5+ 8-5 => 5+1 =6
     // g==h
     // left = 7 , right = 8
     // 7=8
     // mid = 7;
     // h ==h
-    //  mid  7
+    // mid 7
     // trget = "u";
-  /*   
-  target ele = b  , (log(n) log(2)
-  target elem = g , log(2), 
-  h = 3 step , 8 step
-  j 4 step , 9 stewpo
-  8= 2^3
-  ;1lc = 2^1000
- // 1 sec   64 o(64) = 64 10lcksecond
- //  log2(64) => 6 1 step => 6sec 100 second
-  target elete = e => time complexity => O(1) (64) > log(n) 1 l => O() = (log(64))
-  log(9) = 3. 0000 => 4
-    // 
-    //                       {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
-    //                        0    1    2    3    4    5    6    7    8
-    //                                           /  \   
-    //                      {'a', 'b', 'c', 'd']    ['f', 'g', 'h', 'j']
-                        //     0    1    2    3        5    6    7    8
-            //                     / \                      /\
-            //               {'a'}     [ 'c', 'd']      ['f']  ['h', 'j']
-            //                  0          2, 3            5    7     8
-                                       /\                     /\     
-                                                ['d']                  ['j']
-                                                   3                     8
+    /*
+     * target ele = b , (log(n) log(2)
+     * target elem = g , log(2),
+     * h = 3 step , 8 step
+     * j 4 step , 9 stewpo
+     * 8= 2^3
+     * ;1lc = 2^1000
+     * // 1 sec 64 o(64) = 64 10lcksecond
+     * // log2(64) => 6 1 step => 6sec 100 second
+     * target elete = e => time complexity => O(1) (64) > log(n) 1 l => O() =
+     * (log(64))
+     * log(9) = 3. 0000 => 4
+     * //
+     * // {'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'j'};
+     * // 0 1 2 3 4 5 6 7 8
+     * // / \
+     * // {'a', 'b', 'c', 'd'] ['f', 'g', 'h', 'j']
+     * // 0 1 2 3 5 6 7 8
+     * // / \ /\
+     * // {'a'} [ 'c', 'd'] ['f'] ['h', 'j']
+     * // 0 2, 3 5 7 8
+     * /\ /\
+     * ['d'] ['j']
+     * 3 8
+     * 
+     * 
+     */
 
-
-            */ 
- 
- 
- 
-        public static int BinarySearch(char arr[], char target){
+    public static int BinarySearch(char arr[], char target) {
         // 0 to n-1
-         int left =0;
-         int right = arr.length-1;
-         while(left<=right){
-            int mid= left + (right-left)/2;
-            if(arr[mid]==target){
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
                 return mid;
             }
-            if(target> arr[mid]){
-                left = mid +1;
-            }else{
-                right = mid -1;
+            if (target > arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-         }
+        }
 
         return -1;
     }
-    
+
 }
--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -47,17 +47,28 @@ public class SinglyLinkedList {
         }
     }
 
+    // n1, n2, n3.    n4
+
+    // k==1, 0nodes, 
+    // k==1, 1node
+    // k=1 more node;
     public void insertionAtKthPostion( int data ,int k){
         Node newnode = new Node(data);
         if(k <=0){
             return ;
         }else if(k ==1  && head == null){
-            insertionAtLast(data);
-        }else{
+               Node newNode = new Node(data);
+               head =newNode;
+        } else if(k==1 && head !=null){
+             Node newNode = new Node(data);
+             newNode.next = head;
+             head = newNode;
+        }
+        else{
             Node newNode = new Node(data);
             Node currNode = head;
             k = k-1;
-            while(k>0){
+            while(k>0 ){
                 currNode= currNode.next;
                 k--;
             }
@@ -65,16 +76,94 @@ public class SinglyLinkedList {
             currNode.next= newNode;
         }
     }
+    // 0 nnode
+    // 1 node 
+    // more than one node  // 2 2  
 
     public void deletioinAtLast(){
 
+            if(head ==null){
+                return;
+            }else if(head.next ==null){
+                head =null;
+                return;
+            }else{
+                Node current = head;
+                Node current2 = current.next;
+
+                while(current2.next !=null){
+                    current= current.next;
+                    current2 = current2.next;
+                }
+
+                // while(current.next.next !=null){ // current2.next === currrent.next.next
+                //     current = current.next;
+                // }
+                current.next= null;
+            }
+
     }
 
-    public void deletionAtKthPosition(){
-        
+    // k<=0 
+    // k=1, node 1  head =null   k =1, node =1
+    //k=1 node more 1, k=1. n1, n2, n3
+    //  
+    public void deletionAtKthPosition(int k){
+        if(k<=0){
+            return;
+        }
+        if(k ==1){
+            head= head.next;
+            return;
+        }
+        else{
+            Node currentNode = head;
+            Node prev = null;
+            k= k-1;
+            while(k>0){
+                 prev = currentNode;
+                currentNode= currentNode.next;
+                k--;
+            }
+            prev.next = currentNode.next;
+        }
     }
+
+    // n1 n2 n3 n4
+
+         public void display(){
+
+            if(head == null){
+                System.out.println("Linked List is empty");
+                return;
+            }
+            Node curNode = head;
+            while(curNode != null){
+                System.out.println(curNode.data + "  data ");
+                curNode = curNode.next;
+            }
+            
+        }
+
      public static void main(String[] args) {
-        
+
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.insertionAtLast(1);
+         linkedList.insertionAtLast(2);
+          linkedList.insertionAtLast(3);
+           linkedList.insertionAtLast(4);
+            linkedList.deletioinAtLast();
+            linkedList.display();
+                 System.out.println("----------------");
+            linkedList.insertionAtKthPostion(5, 2);
+             linkedList.insertionAtKthPostion(6, 1);
+             linkedList.display();
+                  System.out.println("--------------");
+             linkedList.deletionAtKthPosition(3);
+              linkedList.display();
+                System.out.println("--------------");
+           linkedList.deletionAtKthPosition(1);
+              linkedList.display();
     }
-    
 }
